c7e9cb9fef12411ce7e462ee790432e0dd07fe89
le phung <lephung@les-MacBook-Pro.local>
2020-06-05 15:27:27 +0700

    finish project

--- todolist-1/src/Components/ListItem.js
+++ todolist-1/src/Components/ListItem.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import "./ListItem.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
 export default function ListItem(props) {
     const items = props.items || [];
     const ListItems = items.map((item) => {
         return (<div className="list" key={item.key}>
             <p>
-                <input type="text" 
-                    value={item.text} 
+                <input type="text"
+                    value={item.text}
                     id={item.key}
                     onChange={
-                        (e)=>{
-                        props.setUpdate(e.target.value, item.key)
-                    }}/>
+                        (e) => {
+                            props.setUpdate(e.target.value, item.key)
+                        }} />
                 <span>
                     <FontAwesomeIcon className="faicons" icon="trash"
                         onClick={() => props.deleteItems(item.key)}
@@ -21,10 +22,13 @@ export default function ListItem(props) {
             </p>
         </div>);
     });
-    
+
     return (
         <div>
-            {ListItems}
+            <FlipMove duration={500} easing="ease-in-out">
+                {ListItems}
+            </FlipMove>
+
         </div>
     );
 }
