e4c18def50d8b1e664b421499496e67576568e53
le phung <lephung@les-MacBook-Pro.local>
2020-06-11 10:33:24 +0700

    Updates

---
+++ precache-manifest.aa747ebeb92602d54bfcc4af16240747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57aa71535ff47851078b1a20dc7e9f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0f59f8c4fbaf56a094c",
+    "url": "/static/css/main.9bdf8069.chunk.css"
+  },
+  {
+    "revision": "61151f3fe28ccb5cc5cb",
+    "url": "/static/js/2.a2e51d31.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.a2e51d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f59f8c4fbaf56a094c",
+    "url": "/static/js/main.2894a880.chunk.js"
+  },
+  {
+    "revision": "f0ee4e1beb52f07e95f1",
+    "url": "/static/js/runtime-main.b3208474.js"
+  }
+]);
